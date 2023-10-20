8220e39af67e471cee18bd7b00e6fbdf8d4e4a23
Admin <Admin@TEB123016>
2023-10-20 14:53:57 +0200

    Add project files.

---
+++ uppgift 3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace uppgift_3._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder > 19)
+            {
+                Console.WriteLine("du är för gammal");
+                Console.ReadKey();  
+            }
+            if (ålder < 16)
+            {
+                Console.WriteLine("du är för ung");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Du får vara med");
+                Console.ReadKey();  
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
